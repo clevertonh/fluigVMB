e51f42e710544695599da4e5b34a33b1d7a421f3
wasleysantos <wasleyguittar@gmail.com>
2019-10-11 18:09:57 -0300

    Ajustes visao mundial

--- Fluig VMB Teste/datasets/Testes/ds_post_FINA050.js
+++ Fluig VMB Teste/datasets/Testes/ds_post_FINA050.js
@@ -1,17 +1,17 @@
 function createDataset(fields, constraints, sortFields) {
 	var constraint = new Array();
 	
-	constraint.push(DatasetFactory.createConstraint("documentid", "2211", "2211", ConstraintType.MUST));     
+	constraint.push(DatasetFactory.createConstraint("documentid", "9723", "9723", ConstraintType.MUST));     
 	 
 	
 	
-	 var valorDiarias="300";
-	 var dataVencimento="07/09/2019";	 	
+	 var valorDiarias="212.50";
+	 var dataVencimento="02/10/2019";	 	
 	 constraint.push(DatasetFactory.createConstraint("vl_tarifa", valorDiarias, valorDiarias, ConstraintType.MUST));  
 	 constraint.push(DatasetFactory.createConstraint("dtTarifa", dataVencimento, dataVencimento, ConstraintType.MUST));
 	
 	
-    var dataset = DatasetFactory.getDataset("VM_FINA100_SOLICITACAO_DIARIAS", null, constraint, null);
+    var dataset = DatasetFactory.getDataset("VM_FINA050_SOLICITACAO_VIAGEM", null, constraint, null);
     
     return dataset;
 	
