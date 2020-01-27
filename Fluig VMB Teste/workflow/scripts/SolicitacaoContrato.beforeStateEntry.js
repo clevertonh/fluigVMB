22a77b69d7be98f7db6fce60b52b54b3e7c3efa2
wasleysantos <wasleyguittar@gmail.com>
2020-01-27 17:08:41 -0300

    Ajustes contratos

--- Fluig VMB Teste/workflow/scripts/SolicitacaoContrato.beforeStateEntry.js
+++ Fluig VMB Teste/workflow/scripts/SolicitacaoContrato.beforeStateEntry.js
@@ -3,6 +3,8 @@ function beforeStateEntry(sequenceId){
 	var SOLICITAR = 4;
 	var ELABORAR =10;
 	var ASSINAR = 18;
+	var ANEXAR = 44;
+	var ADITIVO = 49;
 	
 	var ativAtual 		 = getValue("WKNumState");		
 	var codSolicitacao 	 = getValue("WKNumProces");
@@ -13,18 +15,27 @@ function beforeStateEntry(sequenceId){
     var usuario = getValue('WKUser');
 	var definicaoValor = hAPI.getCardValue("definicaoValor");	
 	var contrato = hAPI.getCardValue("Numerocontrato");
-	var statusContrato = hAPI.getCardValue("statusContrato");	   
+	var statusContrato = hAPI.getCardValue("statusContrato");
+	var tipoContrato = hAPI.getCardValue("tipoContrato");	   
     
     if (ativAtual == ASSINAR){
     	if (nextAtv == 37){
-    		//O CONTRATO FOI ASSINADO
-			if (statusContrato =="assinado"){				
+    		//O CONTRATO FOI ASSINADO E UM NOVO CONTRATO
+			if (statusContrato =="assinado" && tipoContrato !=""){				
 				setContrato(idDocumento,3); 
 				
 			}
     		
     	}
     }
+    else if (ativAtual == ANEXAR){
+    		//VERIFICA SE EXISTE ANEXO NO CONTRATO
+    		if (tipoContrato != "") {
+    			getAnexosProtheus(idDocumento);
+    		}
+    }
+    
+    
     
     
     function setContrato(id,opcao){
@@ -40,6 +51,17 @@ function beforeStateEntry(sequenceId){
 		        	 	hAPI.setTaskComments(usuario, codSolicitacao, 0, "Contrato registrado automaticamente. Número: " + resultDataset.getValue(0,"NUMERO"));
 		         }
     
+    }
+    
+    function getAnexosProtheus(id){
+		 var constraints = new Array();                                 
+		 constraints.push(DatasetFactory.createConstraint("documentid", id, id, ConstraintType.MUST));	
+	     var resultDataset = DatasetFactory.getDataset("VM_FATA340_BASE_CONHECIMENTO_PROTHEUS", null, constraints, null);                                                                    
+	    		     
+	         if (resultDataset.getValue(0,"RETORNO") != "SUCESSO"){
+	             throw resultDataset.getValue(0,"RETORNO");
+	         }	        
+
     }
     
     
